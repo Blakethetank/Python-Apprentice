b097063d6f5e67e464e9eae218c1a79a08eb7c02
Blakethetank <jolenef3@yahoo.com>
2024-09-24 22:47:58 +0000

    I worked on "meet Tina 0 and 1

--- lessons/00_Turtles/02_Meet_Tina.py
+++ lessons/00_Turtles/02_Meet_Tina.py
@@ -18,11 +18,11 @@ tina = turtle.Turtle()                  # Create a turtle named tina
 tina.shape('turtle')                    # Set the shape of the turtle to a turtle
 tina.speed(2)                           # Make the turtle move as fast, but not too fast.
 
-tina.pencolor('blue')                   # Set the pen color to blue
+tina.pencolor('green')                   # Set the pen color to blue
 tina.forward(150)                       # Move tina forward by the forward distance
 tina.left(90)                           # Turn tina left by the left turn
 
-tina.pencolor('red')                    # Set the pen color to red
+tina.pencolor('green')                    # Set the pen color to red
 tina.forward(150)                       # Continuie the last two steps three more times
 tina.left(90)                           # to draw a square
 
@@ -38,16 +38,18 @@ tina.penup()                            # Lift the pen up so we can move tina wi
 tina.forward(20)                        # Move tina forward by 20
 tina.left(90)                           # Turn tina left by 90 degrees
 tina.forward(20)                        # Move tina forward by 20
-tina.write("Why, hello there!")         # Write the message "Why, hello there!"
+tina.write("Weasly has tooth!A wonderful magic tooth!")         # Write the message "Why, hello there!"
 tina.backward(20)                       # Move tina backward by 20
 
 tina.goto(-50,0)
 tina.pendown()
-tina.color('red')                       # Set the color of tina to red
-tina.begin_fill()
-tina.circle(101, steps=50)
-tina.end_fill()
 
+tina.color('black')                       # Set the color of tina to red
+tina.begin_fill()
+tina.circle(200 )
+tina.end_fill()
+tina.right(90)
+tina.forward(280)
 turtle.exitonclick()                    # Close the window when we click on it
 
 # Now you can try writing your own programs. Open
